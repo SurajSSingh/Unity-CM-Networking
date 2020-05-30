0808078c3f8df263859e50f92b478c9fd7964d05
Suraj <surajss@uci.edu>
2020-05-30 16:57:42 -0700

    Added GameManager

---
+++ Assets/Scripts_S/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // This is for the quit the quit button
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
