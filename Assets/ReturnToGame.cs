edbbc63bacc78c89308020ef7dc0cba9aefd5070
RMF-AndyPlayz <andyjia2500@gmail.com>
2020-05-30 17:40:25 -0700

    Indev Update 5

---
+++ Assets/ReturnToGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButton(0)){
+            SceneManager.LoadScene("Room 2 Pong");
+        }
+    }
+}
