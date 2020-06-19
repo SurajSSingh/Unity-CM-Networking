0c946316ed2648ffd8a4c18ee98728571babd67b
RMF-AndyPlayz <andyjia2500@gmail.com>
2020-06-18 19:27:23 -0700

    Indev Update 8

--- Assets/Scenes/Room 2 Pong/Scripts/Pong_Ball.cs
+++ Assets/Scenes/Room 2 Pong/Scripts/Pong_Ball.cs
@@ -4,9 +4,75 @@ using UnityEngine;
 
 public class Pong_Ball : MonoBehaviour
 {
-    
-    
+    [SerializeField]
+    public float speed;
 
+    float radius;
+
+
+    Vector2 direction;
+
+    void Start()
+    {
+        direction = Vector2.one.normalized;
+
+        radius = transform.localScale.x / 2;
+    }    
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (transform.position.y < GameManager_GameConfig.bottomLeft.y + radius && direction.y <0)
+        {
+            direction.y = -direction.y;
+        }
+
+        if (transform.position.y > GameManager_GameConfig.topRight.y - radius && direction.y > 0)
+        {
+            direction.y = -direction.y;
+        }
+
+
+
+
+
+        //If someone wins:
+        if (transform.position.x < GameManager_GameConfig.bottomLeft.x + radius && direction.x < 0)
+        {
+            Debug.Log("Player on the right wins!");
+            Time.timeScale = 0;
+        }
+
+        if (transform.position.x > GameManager_GameConfig.topRight.x - radius && direction.x > 0)
+        {
+            Debug.Log("Player on the left wins!");
+            Time.timeScale = 0;
+        }
+
+
+    }
+
+
+    //Making the ball bounce when hit with paddle
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            bool isRight = other.GetComponent<Pong_Paddles>().isRight;
+
+
+            if (isRight == true && direction.x > 0)
+            {
+                direction.x = -direction.x;
+            }
+
+            if (isRight == false && direction.x < 0)
+            {
+                direction.x = -direction.x;
+            }
+        }
+    }
 
 
 }
--- Assets/Scenes/Room 2 Pong/Scripts/Pong_Paddles.cs
+++ Assets/Scenes/Room 2 Pong/Scripts/Pong_Paddles.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Pong_Paddles : MonoBehaviour
 {
+    [SerializeField]
+    public float speed;
+
+    string input;
+    public bool isRight;
+    float height;
+
+    void Start()
+    {
+        height = transform.localScale.y;
+    }
+
     //This function will let GamaManager_GameConfig access all yer memes.
     public void Init(bool isRightPlayer)
     {
-
+        isRight = isRightPlayer;
         Vector2 pos = Vector2.zero;
 
 
@@ -15,11 +27,43 @@ public class Pong_Paddles : MonoBehaviour
         //Creates memes on the right of the screen
         {
             pos = new Vector2(GameManager_GameConfig.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x;
+            input = "Right Player";
         }
         else //Creates memes on the left of the screen 
         {
             pos = new Vector2(GameManager_GameConfig.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+            input = "Left Player";
         }
         transform.position = pos;
+        transform.name = input;
+
     }
+
+    void Update()
+    {
+        //Move the players:
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        if (transform.position.y < GameManager_GameConfig.bottomLeft.y + height / 2 && move <0)
+        {
+            move = 0;
+        }
+
+        if (transform.position.y > GameManager_GameConfig.topRight.y - height / 2 && move > 0)
+        {
+            move = 0;
+        }
+
+
+        transform.Translate(move * Vector2.up);
+
+
+    }
+
+
+
+
+
 }
