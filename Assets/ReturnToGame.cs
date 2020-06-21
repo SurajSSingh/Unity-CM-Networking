5e952f96247ea617c8943a4c9e67a6b240b71c48
Suraj <surajss@uci.edu>
2020-06-20 17:51:25 -0700

    Added simple menu

---
+++ Assets/ReturnToGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToGame : MonoBehaviour
+{
+
+    public GameObject PauseMenu;
+
+    void Start()
+    {
+        CloseMenu();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            PauseMenu.SetActive(true);
+        }
+        Debug.Log(Input.GetKey(KeyCode.Escape));
+    }
+
+    public void CloseMenu()
+    {
+        PauseMenu.SetActive(false);
+    }
+}
---
+++ Assets/Scripts_S/MainMenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public List<GameObject> gameLobbies = new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject lobby in gameLobbies)
+        {
+            lobby.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OpenGameLobby(int gameID)
+    {
+        if (gameID >= 0 && gameID < gameLobbies.Count)
+        {
+            gameLobbies[gameID].SetActive(true);
+        }
+    }
+
+    public void CloseGameLobby(int gameID)
+    {
+        if (gameID >= 0 && gameID < gameLobbies.Count)
+        {
+            gameLobbies[gameID].SetActive(false);
+        }
+    }
+
+    public void UpdateName(string name)
+    {
+        PlayerPrefs.SetString("name", name);
+    }
+
+    public void QuitGame()
+    {
+        // Deletes name and other values when exiting the game
+        PlayerPrefs.DeleteAll();
+        Application.Quit();
+    }
+}
