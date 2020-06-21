95dddca6b0a58da301a7536ff86d2bdcc0cd0973
thicczucc <56744122+thicczucc@users.noreply.github.com>
2020-06-20 17:50:39 -0700

    my push

---
+++ Assets/Scripts_S/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Bullet", menuName = "CustomObject/Bullet", order = 2)]
+
+public class Bullet : ScriptableObject
+{
+    public string Name;
+    public float Damage;
+    public float Velocity;
+}
+
---
+++ Assets/Scripts_S/Gun.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun", menuName = "CustomObject/Gun", order = 1)]
+
+public class Gun : ScriptableObject
+{
+    public string Name;
+    public float Firerate;
+    public int AmmoCount;
+    public Bullet bullet;
+    public int Cooldown;
+}
+
