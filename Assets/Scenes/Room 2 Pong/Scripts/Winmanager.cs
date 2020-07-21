2c657fc5fc3934a5b84e5c2ef349b74e09698a6b
RMF-AndyPlayz <andyjia2500@gmail.com>
2020-07-21 15:43:53 -0700

    Indev Update 13

---
+++ Assets/Scenes/Room 2 Pong/Scripts/Winmanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winmanager : MonoBehaviour
+{
+    public GameObject winScreen;
+    public GameObject leftWinner;
+    public GameObject rightWinner;
+
+
+
+    void Start()
+    {
+        winScreen.SetActive(false);
+        leftWinner.SetActive(false);
+        rightWinner.SetActive(false);
+    }
+
+    public void WinGame(bool leftWin)
+    {
+        winScreen.SetActive(true);
+        if (leftWin)
+        {
+            leftWinner.SetActive(true);
+        }
+        else
+        {
+            rightWinner.SetActive(true);
+        }
+        
+    }
+}
