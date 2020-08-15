fbefe53160a10f02f8e85adad5b889133745c6b6
thicczucc <56744122+thicczucc@users.noreply.github.com>
2020-08-15 16:16:29 -0700

    network instantiation

---
+++ Assets/Scripts_S/ShooterGameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ShooterGameManager : MonoBehaviourPunCallbacks
+{
+    public List<Transform> spawnpoints;
+    public GameObject ninjaPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spawnpoints.Count >= 3)
+        {
+            GameObject ninja = PhotonNetwork.Instantiate(ninjaPrefab.name, spawnpoints[0].position, spawnpoints[0].rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
